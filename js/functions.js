e452870f6d796c892966204c23407255e6a0faaf
Ксения <easyghoul@gmail.com>
2023-10-09 14:32:35 +0700

    Добавляет функцию извлечения чисел из строк

--- js/functions.js
+++ js/functions.js
@@ -35,8 +35,31 @@ const isPalindrome = (string) => {
   return result;
 };
 
+// Проверка работы функции
 console.log('Ожидаю true, получаю - ', isPalindrome('А лис он умен крыса сыр к нему носила'));
 console.log('Ожидаю true, получаю - ', isPalindrome('ДовОд'));
 console.log('Ожидаю true, получаю - ', isPalindrome('а р о з а упала на л а п у   аз о ра'));
 console.log('Ожидаю false, получаю - ', isPalindrome('Кекс'));
 console.log('Ожидаю false, получаю - ', isPalindrome('горошек'));
+
+// Функция извлечения чисел из строк
+const pullingNumber = (value) => {
+  const valueString = value.toString();
+  let number = '';
+
+  for (let i = 0; i < valueString.length; i++) {
+    if (!Number.isNaN(parseInt(valueString[i], 10))) {
+      number += valueString[i];
+    }
+  }
+
+  return parseInt(number, 10);
+};
+
+// Проверка работы функции
+console.log(pullingNumber(2023));
+console.log(pullingNumber('ECMAScript 2022'));
+console.log(pullingNumber('1 кефир, 0.5 батона'));
+console.log(pullingNumber('агент 007'));
+console.log(pullingNumber('а я томат'));
+console.log(pullingNumber(-1));
