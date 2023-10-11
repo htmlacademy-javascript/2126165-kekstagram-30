ce0e8bd4914b21176980ea4e2fc984303c29426c
Ксения <easyghoul@gmail.com>
2023-10-11 09:41:47 +0700

    Добавляет заглушку для вызова функций

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,6 @@
 // Функция для проверки длины строки
 const validateMaxLength = (text, maxLength) => text.length <= maxLength;
 
-validateMaxLength('Hello!', 20); // true
-validateMaxLength('Hello', 444444); // true
-validateMaxLength('Hello', 4); // false
-validateMaxLength('Hellooooooo', 4); // false
-
 // Функция для проверки, является ли строка палиндромом
 const isPalindrome = (sequence) => {
   sequence = String(sequence).toLowerCase().replaceAll(' ', '');
@@ -17,13 +12,6 @@ const isPalindrome = (sequence) => {
 
   return reversed === sequence;
 };
-
-isPalindrome('А лис он умен крыса сыр к нему носила'); // true
-isPalindrome('ДовОд'); // true
-isPalindrome('а р о з а упала на л а п у   аз о ра'); // true
-isPalindrome(12321); // true
-isPalindrome('Кекс'); // false
-isPalindrome('горошек'); // false
 
 // Функция извлечения чисел из строк
 const parseDigits = (value) => {
@@ -39,9 +27,4 @@ const parseDigits = (value) => {
   return parseInt(result, 10);
 };
 
-parseDigits(2023); // 2023
-parseDigits('ECMAScript 2022'); // 2022
-parseDigits('1 кефир, 0.5 батона'); // 105
-parseDigits('агент 007'); // 7
-parseDigits('а я томат'); // NaN
-parseDigits(-1); // 1
+void(validateMaxLength, isPalindrome, parseDigits);
