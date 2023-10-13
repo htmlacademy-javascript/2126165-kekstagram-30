7356aa55ea6742835a3a87bf993e9abb086613d1
Ксения <easyghoul@gmail.com>
2023-10-13 17:49:00 +0700

    Добавляет функцию для получения случайного целого в диапазоне

--- js/main.js
+++ js/main.js
@@ -4,4 +4,9 @@ const getRandomItem = (items) => {
   return items[index];
 };
 
-void(getRandomItem);
+const getRandomInteger = (min, max) => {
+  const fraction = (max - min) * Math.random() + min;
+  return Math.round(fraction);
+};
+
+void(getRandomItem, getRandomInteger);
