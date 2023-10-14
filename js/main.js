a274ff339ababa5cee8b9e554592c9e6d937fe05
Ксения <easyghoul@gmail.com>
2023-10-14 10:16:03 +0700

    Добавляет функцию создания массива комментариев

--- js/main.js
+++ js/main.js
@@ -9,6 +9,18 @@ const getRandomInteger = (min, max) => {
   return Math.round(fraction);
 };
 
+const createCommentsData = (itemCount) => {
+  const messages = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+  const names = ['Александр', 'Виолетта', 'Джокер', 'Елена', 'Изабелла', 'Татьяна', 'Леонардо', 'Серафима', 'Максим', 'Флоренция', 'Пётр', 'Валентина', 'Рюрик', 'Любовь', 'Всеволод', 'Варвара'];
+
+  return new Array(itemCount).fill(1).map((start, index) => ({
+    id: start + index,
+    avatar: `img/avatar-${getRandomInteger(1, 6)}.svg`,
+    message: getRandomItem(messages),
+    name: getRandomItem(names),
+  }));
+};
+
 const createPicturesData = (itemCount = 25) => {
   const descriptions = ['Описание1', 'Описание2', 'Описание3'];
 
@@ -17,8 +29,8 @@ const createPicturesData = (itemCount = 25) => {
     url: `photos/${start + index}.jpg`,
     description: getRandomItem(descriptions),
     likes: getRandomInteger(15, 200),
-    comments: [] // TODO: createCommentsData
+    comments: [createCommentsData(getRandomInteger(0, 30))]
   }));
 };
 
-void(getRandomItem, getRandomInteger, createPicturesData);
+void (getRandomItem, getRandomInteger, createPicturesData, createCommentsData);
