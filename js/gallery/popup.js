ec5ba6b8b359e70aea9f8357ddd8581426f867f0
Ксения <easyghoul@gmail.com>
2023-10-29 08:52:54 +0700

    Добавляет функционал показа полноразмерных изображений

--- js/gallery/popup.js
+++ js/gallery/popup.js
@@ -4,6 +4,31 @@ import {renderComments} from './comments.js';
 const popup = document.querySelector('.big-picture');
 const closeButton = document.querySelector('.big-picture__cancel');
 
+const onDocumentKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closePopup();
+  }
+};
+
+const onCloseButtonClick = () => {
+  closePopup();
+};
+
+function showPopup() {
+  popup.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+  closeButton.addEventListener('click', onCloseButtonClick);
+}
+
+function closePopup() {
+  popup.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  closeButton.removeEventListener('click', onCloseButtonClick);
+}
+
 const renderPopupDetails = (properties) => {
   const {url, description, likes, comments} = properties;
   popup.querySelector('.big-picture__img img').src = url;
@@ -12,3 +37,13 @@ const renderPopupDetails = (properties) => {
   popup.querySelector('.social__caption').textContent = description;
   renderComments(comments);
 };
+
+const onThumbnailClick = () => {
+  document.addEventListener('thumbnailSelect', (evt) => {
+    evt.preventDefault();
+    renderPopupDetails(evt.detail);
+    showPopup();
+  });
+};
+
+export {onThumbnailClick};
