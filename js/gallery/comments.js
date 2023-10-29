b5c291c7d6e94b791e0f38104b07079a8d59a68f
Ксения <easyghoul@gmail.com>
2023-10-29 08:38:19 +0700

    Добавляет функционал создания комментариев

---
+++ js/gallery/comments.js
@@ -0,0 +1,22 @@
+const template = document.querySelector('#comment');
+const container = document.querySelector('.social__comments');
+const counter = document.querySelector('.social__comment-count');
+const loader = document.querySelector('.comments-loader');
+
+const createComment = (comments) => comments.map((properties) => {
+  const {avatar, name, message} = properties;
+  const comment = template.content.querySelector('.social__comment').cloneNode(true);
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+  return comment;
+});
+
+const renderComments = (comments) => {
+  container.querySelectorAll('.social__comment').forEach((commentElement) => commentElement.remove());
+  container.append(...createComment(comments));
+  counter.classList.add('hidden'); // временно скрывает блок счётчика комментариев
+  loader.classList.add('hidden'); // временно скрывает блок загрузки новых комментариев
+};
+
+export {renderComments};
