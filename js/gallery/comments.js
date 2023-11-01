bf126f453be27ca4868f5738a49747d218916e06
Ксения <easyghoul@gmail.com>
2023-11-01 10:46:25 +0700

    Заменяет шаблон комментария на комментарий из разметки

--- js/gallery/comments.js
+++ js/gallery/comments.js
@@ -1,5 +1,5 @@
-const template = document.querySelector('#comment');
 const container = document.querySelector('.social__comments');
+const template = document.querySelector('.social__comment');
 const counter = document.querySelector('.social__comment-count');
 const loader = document.querySelector('.comments-loader');
 
@@ -8,10 +8,12 @@ loader.classList.add('hidden'); // временно скрывает блок з
 
 const createComment = (comments) => comments.map((properties) => {
   const {avatar, name, message} = properties;
-  const comment = template.content.querySelector('.social__comment').cloneNode(true);
+  const comment = template.cloneNode(true);
+
   comment.querySelector('.social__picture').src = avatar;
   comment.querySelector('.social__picture').alt = name;
   comment.querySelector('.social__text').textContent = message;
+
   return comment;
 });
 
