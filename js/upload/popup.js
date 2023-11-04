53fdfe972e09364d06518472a573fb3494266eaa
Ксения <easyghoul@gmail.com>
2023-11-04 17:14:06 +0700

    Отменяет обработчик Escape при фокусе в полях

--- js/upload/popup.js
+++ js/upload/popup.js
@@ -1,9 +1,16 @@
 const popup = document.querySelector('.img-upload__overlay');
 const closeButton = document.querySelector('.img-upload__cancel');
+const hashtagField = document.querySelector('.text__hashtags');
+const descriptionField = document.querySelector('.text__description');
 
 const onDocumentKeydown = (event) => {
   if (event.key.startsWith('Esc')) {
-    closeButton.click();
+
+    if (document.activeElement === hashtagField || document.activeElement === descriptionField) {
+      event.stopPropagation();
+    } else {
+      closeButton.click();
+    }
   }
 };
 
