703d6f07fbae1a820d30c96b6784ff27cc8d3198
Keks <keks@htmlacademy.ru>
2023-11-04 09:13:37 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/gallery/popup.js
@@ -0,0 +1,39 @@
+import {renderComments} from './comments.js';
+
+const popup = document.querySelector('.big-picture');
+const closeButton = document.querySelector('.big-picture__cancel');
+
+const onDocumentKeydown = (event) => {
+  if (event.key.startsWith('Esc')) {
+    closeButton.click();
+  }
+};
+
+const openPopup = () => {
+  popup.classList.remove('hidden');
+  popup.scroll(0, 0);
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+const closePopup = () => {
+  popup.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  document.dispatchEvent(new CustomEvent('popupClose'));
+};
+
+const renderPopup = (properties) => {
+  const {url, description, likes, comments} = properties;
+
+  popup.querySelector('.big-picture__img img').src = url;
+  popup.querySelector('.big-picture__img img').alt = description;
+  popup.querySelector('.likes-count').textContent = likes;
+  popup.querySelector('.social__caption').textContent = description;
+  renderComments(comments);
+  openPopup();
+};
+
+closeButton.addEventListener('click', () => closePopup());
+
+export {renderPopup};
