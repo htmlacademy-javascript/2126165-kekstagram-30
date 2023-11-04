428596f5b31f07b578d06871650267d313519935
Ксения <easyghoul@gmail.com>
2023-11-04 22:07:45 +0700

    Импортирует функцию загрузки изображений в главную точку входа

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 import {createPicturesData} from './data.js';
 import {renderGallery} from './gallery/main.js';
+import {renderUpload} from './upload/main.js';
 
 renderGallery(createPicturesData());
+
+renderUpload();
