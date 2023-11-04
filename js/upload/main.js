256dd69f942b3f2958287f4dc371ef6cf9b57099
Ксения <easyghoul@gmail.com>
2023-11-04 22:07:07 +0700

    Добавляет функцию загрузки изображений с обработчиками

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,2 +1,23 @@
 import {openPopup} from './popup.js';
 import {validateForm, resetForm} from './validation.js';
+
+const form = document.querySelector('.img-upload__form');
+const pictureUpload = document.querySelector('.img-upload__input');
+
+const renderUpload = () => {
+
+  pictureUpload.addEventListener('change', () => {
+    openPopup();
+  });
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validateForm();
+  });
+
+  form.addEventListener('reset', () => {
+    resetForm();
+  });
+};
+
+export {renderUpload};
