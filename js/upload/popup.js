666228f7ef0e38a80987b772afe77c7f10db4ff0
Ксения <easyghoul@gmail.com>
2023-11-06 09:55:27 +0700

    Добавляет функционал закрытия попапов по клику на оверлей

--- js/upload/popup.js
+++ js/upload/popup.js
@@ -18,11 +18,18 @@ const onDocumentKeydown = (event) => {
   }
 };
 
+const onOverlayClick = (event) => {
+  if (event.target === popup) {
+    closeButton.click();
+  }
+};
+
 const openPopup = () => {
   popup.classList.remove('hidden');
   popup.scroll(0, 0);
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onDocumentKeydown);
+  document.addEventListener('click', onOverlayClick);
 };
 
 const closePopup = () => {
@@ -30,6 +37,7 @@ const closePopup = () => {
   document.body.classList.remove('modal-open');
   pictureUpload.value = '';
   document.removeEventListener('keydown', onDocumentKeydown);
+  document.removeEventListener('click', onOverlayClick);
 };
 
 closeButton.addEventListener('click', () => closePopup());
