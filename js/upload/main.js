fb7e59ae4b1888e554d6903d51d9de8f06ed061a
Ксения <easyghoul@gmail.com>
2023-11-06 08:47:22 +0700

    Импортирует функцию работы попапа в main.js

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,23 +1,7 @@
-import {openPopup} from './popup.js';
-import {validateForm, resetForm} from './validation.js';
-
-const form = document.querySelector('.img-upload__form');
-const pictureUpload = document.querySelector('.img-upload__input');
+import {renderPopup} from './popup.js';
 
 const renderUpload = () => {
-
-  pictureUpload.addEventListener('change', () => {
-    openPopup();
-  });
-
-  form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    validateForm();
-  });
-
-  form.addEventListener('reset', () => {
-    resetForm();
-  });
+  renderPopup();
 };
 
 export {renderUpload};
