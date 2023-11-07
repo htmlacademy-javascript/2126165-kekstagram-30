20cae22b828a2343d4db69c7c6a597da246068fd
Ксения <easyghoul@gmail.com>
2023-11-07 08:31:32 +0700

    Добавляет и экспортирует функции изменения масштаба

---
+++ js/upload/scale.js
@@ -0,0 +1,25 @@
+const scale = document.querySelector('.img-upload__scale');
+const scaleValue = scale.querySelector('.scale__control--value');
+const picture = document.querySelector('.img-upload__preview img');
+
+let currentScaleValue = parseInt(scaleValue.value, 10);
+
+const transformPicture = () => {
+  picture.style.transform = `scale(${currentScaleValue / 100})`;
+};
+
+const onSmallerClick = () => {
+  if (currentScaleValue > 25) {
+    scaleValue.value = `${currentScaleValue -= 25}%`;
+    transformPicture();
+  }
+};
+
+const onBiggerClick = () => {
+  if (currentScaleValue < 100) {
+    scaleValue.value = `${currentScaleValue += 25}%`;
+    transformPicture();
+  }
+};
+
+export {onSmallerClick, onBiggerClick};
