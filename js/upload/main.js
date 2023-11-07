af6e446d966caa95ebe5761fe5647d9c0342f614
Ксения <easyghoul@gmail.com>
2023-11-07 15:21:30 +0700

    Добавляет функционал эффектов для изображения в точку входа

--- js/upload/main.js
+++ js/upload/main.js
@@ -1,7 +1,7 @@
 import {openPopup} from './popup.js';
 import {validateForm, resetForm} from './validation.js';
 import {currentScaleValue, resetScale} from './scale.js';
-import {renderEffect, resetEffect} from './effects.js';
+import {renderEffect, resetEffect, filter} from './effects.js';
 
 const form = document.querySelector('.img-upload__form');
 const picture = document.querySelector('.img-upload__preview img');
@@ -13,6 +13,14 @@ const transformPictureScale = () => {
 
 const resetPictureScale = () => {
   picture.style.transform = 'none';
+};
+
+const createPictureFilter = () => {
+  picture.style.filter = filter;
+};
+
+const resetPictureFilter = () => {
+  picture.style.filter = 'none';
 };
 
 const resetSlider = () => sliderContainer.classList.add('hidden');
@@ -28,6 +36,7 @@ form.addEventListener('reset', () => {
   resetScale();
   resetPictureScale();
   resetEffect();
+  resetPictureFilter();
 });
 
 form.addEventListener('change', (event) => {
@@ -37,6 +46,8 @@ form.addEventListener('change', (event) => {
       return openPopup();
     case 'effect':
       return renderEffect(event.target.id);
+    case 'effect-level':
+      return createPictureFilter();
     case 'scale':
       return transformPictureScale();
   }
