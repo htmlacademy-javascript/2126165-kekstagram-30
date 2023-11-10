3e4ae2ca09aaea8dd1bd0aa38a6dc732ad30af96
Ксения <easyghoul@gmail.com>
2023-11-10 10:54:34 +0700

    Добавляет функционал отправки данных

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 import {renderGallery} from './gallery/main.js';
 import {request} from './utilities.js';
+import {renderStatus} from './status.js';
+import {resetForm} from './upload/main.js';
+import {closePopup} from './upload/popup.js';
 
 const GET_URL = 'https://30.javascript.pages.academy/kekstagram/data';
 const SEND_URL = 'https://30.javascript.pages.academy/kekstagram';
@@ -7,4 +10,18 @@ const SEND_URL = 'https://30.javascript.pages.academy/kekstagram';
 request(GET_URL, {
   method: 'GET'
 })
-  .then((data) => renderGallery(data));
+  .then((data) => renderGallery(data))
+  .catch(() => renderStatus('data-error'));
+
+document.addEventListener('formdata', (event) => {
+  request(SEND_URL, {
+    method: 'POST',
+    body: event.formData
+  })
+    .then(() => {
+      renderStatus('success');
+      resetForm();
+      closePopup();
+    })
+    .catch(() => renderStatus('error'));
+});
