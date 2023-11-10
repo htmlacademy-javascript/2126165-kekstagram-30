102847b91431948586e56e901a21108608490fc7
Ксения <easyghoul@gmail.com>
2023-11-10 10:36:47 +0700

    Отрисовывает данные, полученные с сервера

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 import {renderGallery} from './gallery/main.js';
-import './upload/main.js';
+import {request} from './utilities.js';
 
+const GET_URL = 'https://30.javascript.pages.academy/kekstagram/data';
+const SEND_URL = 'https://30.javascript.pages.academy/kekstagram';
+
+request(GET_URL, {
+  method: 'GET'
+})
+  .then((data) => renderGallery(data));
