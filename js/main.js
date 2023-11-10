e7367ec5d3b7463201328ec410a5129152f673c8
Ксения <easyghoul@gmail.com>
2023-11-10 10:34:03 +0700

    Удаляет функционал отрисовки данных вручную

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
-import {createPicturesData} from './data.js';
 import {renderGallery} from './gallery/main.js';
 import './upload/main.js';
-
-renderGallery(createPicturesData());
 
--- js/utilities.js
+++ js/utilities.js
@@ -8,15 +8,4 @@ const request = async (url, options) => {
   return response.json();
 };
 
-const getRandomItem = (items) => {
-  const fraction = items.length * Math.random();
-  const index = Math.floor(fraction);
-  return items[index];
-};
-
-const getRandomInteger = (min, max) => {
-  const fraction = (max - min) * Math.random() + min;
-  return Math.round(fraction);
-};
-
-export {request, getRandomItem, getRandomInteger};
+export {request};
