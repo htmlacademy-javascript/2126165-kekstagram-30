9eada42e9df7c1bf6e138bc774e1ad466bdb4588
Ксения <easyghoul@gmail.com>
2023-11-10 10:11:23 +0700

    Добавляет функцию для отправки сетевых запросов

--- js/utilities.js
+++ js/utilities.js
@@ -1,3 +1,13 @@
+const request = async (url, options) => {
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+
+  return response.json();
+};
+
 const getRandomItem = (items) => {
   const fraction = items.length * Math.random();
   const index = Math.floor(fraction);
@@ -9,4 +19,4 @@ const getRandomInteger = (min, max) => {
   return Math.round(fraction);
 };
 
-export {getRandomItem, getRandomInteger};
+export {request, getRandomItem, getRandomInteger};
