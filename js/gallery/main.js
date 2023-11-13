795a4f51c875304efc9af5be6a0868acfdec0fbd
Ксения <easyghoul@gmail.com>
2023-11-13 19:44:36 +0700

    Импортирует функции для работы с фильтрами

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -1,5 +1,7 @@
 import {renderThumbnails} from './thumbnails.js';
 import {renderPopup} from './popup.js';
+import {renderFilters} from './filters.js';
+import {applyFilters} from './data.js';
 
 const renderGallery = (picturesData) => {
   renderThumbnails(picturesData);
