8effe1b7854580910b1fdd23947f050ee084e4c3
Ксения <easyghoul@gmail.com>
2023-11-13 19:45:15 +0700

    Добавляет функционал фильтрации изображений в точку входа

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -4,7 +4,12 @@ import {renderFilters} from './filters.js';
 import {applyFilters} from './data.js';
 
 const renderGallery = (picturesData) => {
+  renderFilters();
   renderThumbnails(picturesData);
+
+  document.addEventListener('filterSelect', (event) => {
+    renderThumbnails(applyFilters(picturesData, event.detail));
+  });
 
   document.addEventListener('thumbnailSelect', (event) => {
     renderPopup(event.detail);
