9e7b75ace593c3935d17c8d190f7bd850a091fd7
Ксения <easyghoul@gmail.com>
2023-11-14 09:38:55 +0700

    Добавляет подстановку изображениия пользователя в превью эффектов

--- js/upload/main.js
+++ js/upload/main.js
@@ -6,6 +6,7 @@ import {setEffect, getEffectValue, resetEffect} from './effects.js';
 const form = document.querySelector('.img-upload__form');
 const uploadField = form.querySelector('.img-upload__input');
 const picture = form.querySelector('.img-upload__preview img');
+const effectPreviewPictures = form.querySelectorAll('.effects__preview');
 const submitButton = form.querySelector('.img-upload__submit');
 
 const uploadPicture = (fileExtensions = ['jpg', 'jpeg', 'png']) => {
@@ -14,6 +15,9 @@ const uploadPicture = (fileExtensions = ['jpg', 'jpeg', 'png']) => {
 
   if (validate) {
     picture.src = URL.createObjectURL(file);
+    effectPreviewPictures.forEach((effectPreviewPicture) => {
+      effectPreviewPicture.style.backgroundImage = `url(${URL.createObjectURL(file)})`;
+    });
   }
 };
 
