074fb192035bc5c9d68efe667a8433d4fd68b795
Ксения <easyghoul@gmail.com>
2023-11-15 09:05:41 +0700

    Переносит функционал изменения фильтров в точку входа галереи

--- js/gallery/main.js
+++ js/gallery/main.js
@@ -1,15 +1,25 @@
 import {renderThumbnails} from './thumbnails.js';
 import {renderPopup} from './popup.js';
 import {renderFilters} from './filters.js';
-import {applyFilters} from './data.js';
+import {applyRandomFilter, applyDiscussedFilter} from './data.js';
 
-const renderGallery = (picturesData) => {
+const renderGallery = (picturesData, reduceFrequency) => {
   renderFilters();
   renderThumbnails(picturesData);
 
-  document.addEventListener('filterSelect', (event) => {
-    renderThumbnails(applyFilters(picturesData, event.detail));
-  });
+  document.addEventListener('filterSelect', reduceFrequency((event) => {
+    switch (event.detail) {
+      case 'filter-default':
+        renderThumbnails(picturesData);
+        break;
+      case 'filter-random':
+        renderThumbnails(applyRandomFilter(picturesData));
+        break;
+      case 'filter-discussed':
+        renderThumbnails(applyDiscussedFilter(picturesData));
+        break;
+    }
+  }));
 
   document.addEventListener('thumbnailSelect', (event) => {
     renderPopup(event.detail);
